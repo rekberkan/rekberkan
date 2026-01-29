c49bfe4e7c6fe54954583c113706fec65fff5204
rekberkan <rekberkan.com@gmail.com>
2026-01-29 08:52:34 +0700

    Delete kahade-backend/run-app.sh

--- kahade-backend/run-app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PORT=5000
-export NODE_ENV=development
-export REDIS_ENABLED=false
-cd kahade-backend
-# Run with ts-node ignoring errors to get it up and running
-export NODE_OPTIONS="--no-warnings"
-npx ts-node -r tsconfig-paths/register --transpile-only src/main.ts
